9a458d9eccc4b933d2e19ba8ed70be47a1390f26
Rayirth2009 <118952959+Rayirth2009@users.noreply.github.com>
2023-01-13 11:49:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 n = int(input("Enter a number: "))
 
+#If the number is even
 def even():
     global n
-#    print(n)
     print(n/2)
     n = n/2
 
+#If the number is odd
 def odd():
     global n
-#    print(n)
     print((3*n) + 1)
     n = 3*n + 1
-
+    
+#To solve the equation
 def solve():
     global n
     if n%2 == 0:
